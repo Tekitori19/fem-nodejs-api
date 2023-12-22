0b4eee5a67242f4e7a077fea7aef9f46810a45b6
Dwcks Dinh <corclan19@gmail.com>
2023-12-22 14:59:19 +0700

    added cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import router from './router'
 import morgan from 'morgan'
+import cors from 'cors'
 import { customLogger } from './examples/middleware/custom_middleware'
 
 const app = express()
@@ -8,11 +9,12 @@ const app = express()
 /**
  * USE MIDDLEWARE
  */
+app.use(cors()) //Cross-Origin Resource Sharing - server use this to know who can access this api(default everybody can access this)
 app.use(morgan('dev')) //Log incoming request
 app.use(express.json()) //Allow client to send JSON
 app.use(express.urlencoded({extended: true})) //Make client to encode url to object instead of string
 //ex: 'google.com?a=1,thing=otherthing' query string behind "?" will become object for us
-app.use(customLogger('Dinh')) //Use custom middleware
+app.use(customLogger('Dwcks Dinh')) //Use custom middleware
 
 app.get('/', (req, res) => {
     res.status(200)
